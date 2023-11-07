aceef804934d404a9cd137e02e6bbe6d1c80bb3b
StefanoIG <stefano20033@gmail.com>
2023-11-06 21:41:39 -0500

    V.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Login from './Login';
+import Registro from './Registro';
 
 function App() {
+  const [showLogin, setShowLogin] = useState(true);
+
+  const toggleForm = () => {
+    setShowLogin(!showLogin);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showLogin ? (
+        <Login toggleForm={toggleForm} />
+      ) : (
+        <Registro toggleForm={toggleForm} />
+      )}
     </div>
   );
 }
